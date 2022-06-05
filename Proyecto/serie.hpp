ec69f380b06ff89185e9e673b3a0d27285536b1d
ernestogithub1 <102172191+ernestogithub1@users.noreply.github.com>
2022-06-05 14:15:21 -0500

    Update serie.hpp

--- Proyecto/serie.hpp
+++ Proyecto/serie.hpp
@@ -1,1 +1,23 @@
+#ifndef serie_hpp
+#define serie_hpp
+#include <stdio.h>
+#include "video.hpp"
 
+class serie: public video{
+private:
+  episodios[5]Episodio;
+  cantidad int
+public:
+  setEpisodio(int,Episodio):void
+  setCantidad(int):void
+
+  getEpisodio(int):Episodio;
+  getCantidad()int;
+  
+calcularPromedio:double
+  
+str():string
+};
+
+
+#endif /* Pizza_hpp */
