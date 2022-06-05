dae05db966ad48ca25679f4b95fe8f06423b1b2f
ernestogithub1 <102172191+ernestogithub1@users.noreply.github.com>
2022-06-05 13:31:06 -0500

    Update pelicula.hpp

--- Proyecto/pelicula.hpp
+++ Proyecto/pelicula.hpp
@@ -1,1 +1,20 @@
+#ifndef pelicula_hpp
+#define pelicula_hpp
 
+#include <stdio.h>
+
+#include <string>
+using namespace std;
+#include "video.hpp"
+
+class pelicula : public video{
+  
+private:
+    int oscares;
+public:
+    Pastel();
+    void dibuja();
+};
+
+
+#endif /* Pastel_hpp */
