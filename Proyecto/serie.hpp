531b5aca83cfa2ec15ef5113182fa3218c3e0967
ernestogithub1 <102172191+ernestogithub1@users.noreply.github.com>
2022-06-07 19:37:36 -0500

    Update serie.hpp

--- Proyecto/serie.hpp
+++ Proyecto/serie.hpp
@@ -8,20 +8,22 @@ class serie: public video{
 private:
   Episodio episodios[5];
   int cantidad;
+  
 public:
   serie();
-  serie(string_id,sring_titulo,int_duracion,string_genero,double_calificacionPromedio);
+  serie(string _id, string _titulo, int _duracion, string _genero, double _calificacionPromedio);
   
-  void setEpisodio(int_index, Episodio_episodio);
-  void setCantidad(int_cantidad);
+void setEpisodio(int_index, Episodio_episodio);
+void setCantidad(int_cantidad);
 
-  Episodio getEpisodio(int_index);
-  int getCantidad();
+Episodio getEpisodio(int_index);
+int getCantidad();
   
 double calcularPromedio();
-  
-string str();
+  string str();
+
+
 };
 
 
-#endif /* Pizza_hpp */
+#endif /* serie_hpp */
