55e2fbd7559daa8e288f2971d101ea139103c75f
ernestogithub1 <102172191+ernestogithub1@users.noreply.github.com>
2022-06-13 09:40:34 -0500

    Update video.cpp

--- Proyecto/video.cpp
+++ Proyecto/video.cpp
@@ -1,1 +1,17 @@
 
+
+#include <iostream>
+using namespace std;
+#include "video.hpp"
+
+int main( )
+{ Rectangulo rect;
+ double area;
+ rect .setLargo(10);
+ rect .setAncho(5);
+ area = rect. calcularArea();
+ cout << rect.str( ) <<
+ "\nEl area es "<< area <<endl;
+ return 0;
+}
+
