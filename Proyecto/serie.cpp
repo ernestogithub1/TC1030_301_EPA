119833d622ec6b835f298ab62eecda49447d4966
ernestogithub1 <102172191+ernestogithub1@users.noreply.github.com>
2022-06-14 04:44:13 -0500

    Update serie.cpp

--- Proyecto/serie.cpp
+++ Proyecto/serie.cpp
@@ -1,43 +1,54 @@
 #include "serie.hpp"
 
 serie::serie():video(){
-  cantidad=0;
+  cantidad = 0;
 }
-serie:serie(string_id,sring_titulo,int_duracion,string_genero,double_calificacionPromedio);
-video(_iD,_titulo,_duracion,_genero,0){
- cantidad=0; 
+
+serie::serie(string _id, string _titulo, int _duracion, string _genero, double_calificacionPromedio);
+video(_iD, _titulo, _duracion, _genero,0){
+  cantidad=0; 
 }
 
 
-void serie::setEpisodio(int_index,episodio_episodio){
-  
+void serie::setEpisodio(int _index,episodio _episodio){}
+void serie::setCantidad(int _cantidad){
+  cantidad = _cantidad;
 }
-void serie::getCantidad(int_index){
+
+
+episodio serie::getEpisodio(int _index){
   episodio ep;
   
   return episodios[_index];
   return episodio();
 }
-
 int serie::getCantdad(){
   return cantidad;
 }
+
+
 
 double serie::calculaPromedio(){
   double acum=0;
   for(int index=0, index < cantidad; index++)
     acum= acum+episodio[index].getCalificacion();
   
-  if(cantidad>0)
-    return acun/cantidad;
-    else 
-      return 0;
-
+  if(cantidad > 0)
+    return acun / cantidad;
+  else 
+    return 0;
 }
-string serie::str(){}
-string acum = "";
-for(int index=0, index < cantidad; index++)
-    acum=acum+episodios[index].str()
+
+string serie::str(){
+  
+  string acum = "\n";
+  for(int index = 0, index < cantidad; index++)
+      acum = acum + episodios[index].str() + '\n'
+    
+    
+  return id + ', ' + titulo + ', ' + to_string(duracion) + ', ' + genero + ', ' + to_string(calificacionPromedio) + acum;
+}
+
 
 
 return iD + + titulo + + 
