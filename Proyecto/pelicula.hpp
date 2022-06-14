e5545dc4f98f96b747cb1de5617679c9302a70e0
ernestogithub1 <102172191+ernestogithub1@users.noreply.github.com>
2022-06-14 02:47:46 -0500

    Update pelicula.hpp

--- Proyecto/pelicula.hpp
+++ Proyecto/pelicula.hpp
@@ -1,8 +1,8 @@
 #ifndef pelicula_hpp
 #define pelicula_hpp
 #include <stdio.h>
-#include <string>
 using namespace std;
+#include <iostream>
 #include "video.hpp"
 
 class pelicula : public video{
@@ -10,11 +10,12 @@ class pelicula : public video{
 private:
     int oscares;
 public:
-  setOscares(int):void
-  getOscares():int;
-  
-str():string
+  pelicula();
+  pelicula(string, string, int, string, double, int);
+
+  string str( );
 };
 
 
 #endif /* pelicula_hpp */
+
