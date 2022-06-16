509f16a52ff1254d4a1ce3db66479ec722171e59
ernestogithub1 <102172191+ernestogithub1@users.noreply.github.com>
2022-06-15 19:38:35 -0500

    Update peliculas.cpp

--- Proyecto/peliculas.cpp
+++ Proyecto/peliculas.cpp
@@ -1,54 +1,118 @@
+// Ernesto Poisot Avila
+// A01734765
+// peliculas.cpp
+
 #include "peliculas.hpp"
+#include "pelicula.hpp"
+#include <sstream>
+#include <fstream>
 
-serie::serie():video(){
-  cantidad = 0;
-}
-
-serie::serie(string _id, string _titulo, int _duracion, string _genero, double_calificacionPromedio);
-video(_iD, _titulo, _duracion, _genero,0){
-  cantidad=0; 
-}
+//constructor
+peliculas::peliculas(){}
 
 
-void serie::setEpisodio(int _index,episodio _episodio){}
-void serie::setCantidad(int _cantidad){
-  cantidad = _cantidad;
-}
-
-
-episodio serie::getEpisodio(int _index){
-  episodio ep;
-  
-  return episodios[_index];
-  return episodio();
-}
-int serie::getCantdad(){
-  return cantidad;
-}
-
-
-
-double serie::calculaPromedio(){
-  double acum=0;
-  for(int index=0, index < cantidad; index++)
-    acum= acum+episodio[index].getCalificacion();
-  
-  if(cantidad > 0)
-    return acun / cantidad;
-  else 
-    return 0;
-}
-
-string serie::str(){
-  
-  string acum = "\n";
-  for(int index = 0, index < cantidad; index++)
-      acum = acum + episodios[index].str() + '\n'
+//resto
+int Peliculas::leerArchivo()
+{
+    //fin-file input
+    fstream fin;
     
-    
-  return id + ', ' + titulo + ', ' + to_string(duracion) + ', ' + genero + ', ' + to_string(calificacionPromedio) + acum;
+    string row[6];
+  
+    string line;
+    string word;
+
+    fin.open("pelicula.csv",ios::in);
+
+    int cantidad=0;
+
+    while (getline(fin,line)){
+
+        stringstream s(line);
+        int iR=0;
+
+        while(getline(s,word, ','))
+        {
+ 
+            row[iR++]= word;
+        }
+
+        arrPtrPeliculas[cantidad]=new pelicula(row[0],     
+        row[1], stoi(row[2]), row[3], stof(row[4]),stoi(row[5]));
+
+        cout << "iD: " << row[0] << "\n";
+        cout << "Titulo: " << row[1] << "\n";
+        cout << "Duracion: " << row[2] << "\n";
+        cout << "Genero: " << row[3] << "\n";
+        cout << "Calificacion: " << row[4] << "\n";
+        cout << "Oscares: " << row[5] << "\n" ;
+        cout << "   " << "\n";
+
+
+        cantidad = cantidad + 1;     
+    }
+    fin.close();
+
+        for(int iR=0; iR<cantidad;iR++){
+            cout<< iR <<"-"<<arrPtrPeliculas[iR]->str()<<endl;
+          }
+    return cantidad;
 }
+    // sets
+    void Peliculas::setPtrPelicula(Pelicula *_ptrPelicula){
+        
+        arrPtrPeliculas[cantidad++]= _ptrPelicula;
+    }
+    void Peliculas::setCantidadPeliculas(int _cantidad){
+        cantidad= _cantidad;
+    }
 
+    // gets
+    Pelicula * Peliculas::getPtrPelicula(string _id){
+       return new Pelicula;
+    }
+    int Peliculas::getCantidadPeliculas(){
+        return cantidad;
+    }
 
+    // resto
+    void Peliculas::reporteTodasLasPeliculas()
+    {
+        double promedio;
+        double acum = 0;
+        for(int iR = 0; iR < cantidad; iR++)
+        {
+        cout << "P" << arrPtrPeliculas[iR]->str() << endl;
+        acum = acum + arrPtrPeliculas[iR]->getCalificacion();
+        }
 
-return iD + + titulo + + 
+        promedio = acum / 35;
+        cout << "El promedio es: " << promedio << endl;
+
+    }
+    void Peliculas::reporteConCalificacion(float _calificacionPromedio){
+        int count=0;
+        for (int iR=0; iR < cantidad; iR++){
+            if (arrPtrPeliculas[iR]-> getCalificacion()==_calificacionPromedio)
+            {
+                cout<< iR<< ' ' << arrPtrPeliculas[iR]->str()<<endl;
+                count++;
+            }
+        }
+        if (count==0)
+        cout<<"No hay peliculas de la calificación deseada: " << _calificacionPromedio <<endl;
+    }
+
+    void Peliculas::reporteGenero(string _genero){
+        int count=0;
+        
+        for (int iR=0; iR < cantidad; iR++){
+            if (arrPtrPeliculas[iR]-> getGenero()== _genero)
+            {
+                cout<< iR<< ' ' << arrPtrPeliculas[iR]->str()<<endl;
+                count++;
+            }
+        }
+        if (count==0)
+        cout<<"No hay peliculas del género: " << _genero <<endl;
+    }
