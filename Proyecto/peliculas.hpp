d87927b7f46686b383ecdf124ba855490e19a4e1
ernestogithub1 <102172191+ernestogithub1@users.noreply.github.com>
2022-06-15 19:15:25 -0500

    Update peliculas.hpp

--- Proyecto/peliculas.hpp
+++ Proyecto/peliculas.hpp
@@ -1,1 +1,34 @@
+#ifndef peliculas_hpp
+#define peliculas_hpp
+#include <stdio.h>
+#include "video.hpp"
+#include "pelicula.hpp"
 
+class peliculas: public video{
+  
+private:
+  pelicula arrPtrPeliculas[MAX_PEL];
+  int cantidad;
+  
+public:
+  peliculas();
+  
+  
+  
+  void setPtrPeliculas(string);
+  void setCantidadPeliculas(int _cantidad);
+
+  pelicula getPtrPeliculas(string _index);
+  int getCantidadPeliculas();
+  
+  double calcularPromedio();
+  string str();
+
+
+};
+
+
+#endif /* peliculas_hpp */
+© 2022 GitHub, Inc.
+Terms
+Priv
