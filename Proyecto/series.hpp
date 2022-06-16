3dcd8bf6448875cb788f6cd2cba0188308d87183
ernestogithub1 <102172191+ernestogithub1@users.noreply.github.com>
2022-06-15 19:40:55 -0500

    Update series.hpp

--- Proyecto/series.hpp
+++ Proyecto/series.hpp
@@ -1,1 +1,44 @@
+// Ernesto Poisot Avila
+// A01734765
+// Series.hpp
 
+#ifndef Series_hpp
+#define Series_hpp
+
+#include <stdio.h>
+#include "Serie.hpp"
+#include <sstream>
+#include <fstream>
+
+const int MAX_SERIES = 50;
+
+
+class Series{
+
+private:
+  Serie *arrPtrSeries[MAX_SERIES];
+  int Cantidad{};
+
+
+public:
+  //constructor
+  Series();
+
+  void leerArchivo();
+
+  //sets
+  void setPtrSerie(Serie *ptr);
+  void setCantidadSeries(int _cantidad);
+
+  //gets
+  Serie* getPtrSerie(string _iD);
+
+  int getCantidadSeries();
+
+  void reporteTodasLasSeries();
+  void reporteConCalificacion(double _calificacion);
+  void reporteGenero(string _genero);
+  void calcularCalificacionSeries();
+};
+
+#endif /* Series_hpp */
